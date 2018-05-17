77ff398dbfdd97bd962daf995ed4bc45de872c76
kobl <kobl.co@protonmail.com>
2018-05-17 21:42:34 +0930

    multiple template blanks

--- card/urls.py
+++ card/urls.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('^$', views.card_list, name='card_list'),
+    re_path('card/', views.card_list, name='card_list'),
+	re_path('^$', views.index, name='index'),
 ]
--- card/views.py
+++ card/views.py
@@ -1,9 +1,12 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Card
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the card index.")
 	
 def card_list(request):
     return render(request, 'card/card_list.html', {})
+
+def index(request):
+    return render(request, 'card/index.html', {})
